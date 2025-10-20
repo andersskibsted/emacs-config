a3ee52ca73b0569b1d041d2c54f5578b68faf0a7
andersskibsted <abo@internettet.email>
2025-10-20 21:03:13 +0200

    2025-10-20 Foerste commit. Restructuring efter rod i org-filen

---
+++ init.el
@@ -0,0 +1,26 @@
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/Users/andersskibsted/Documents/org/agenda/dayplanner.org"
+     "/Users/andersskibsted/Documents/org/agenda/inbox.org"))
+ '(package-selected-packages
+   '(company-org-block consult consult-yasnippet corfu dap-mode
+		       doom-modeline evil-collection evil-escape
+		       evil-nerd-commenter evil-surround
+		       fira-code-mode general ligature lsp-mode
+		       marginalia meow orderless org-modern
+		       org-roam-ui org-super-agenda org-superstar
+		       vertico which-key-posframe yasnippet
+		       yasnippet-classic-snippets yasnippet-snippets))
+ '(safe-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
